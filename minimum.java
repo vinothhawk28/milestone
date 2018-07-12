f3185a329253433abb1ec57a87f184928a536804
Vinoth <38490375+vinothhawk28@users.noreply.github.com>
2018-07-12 12:34:24 +0530

    Create minimum.java

---
+++ minimum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class smallest {
+public static void main(String[] args) {
+	int a,b;
+	int c=0;
+	int max=0;
+	int i,temp=0;
+	Scanner in=new Scanner(System.in);
+	a=in.nextInt();
+	int[] arr=new int[a];
+	for(i=0;i<a;i++){
+		arr[i]=in.nextInt();
+	}
+	Arrays.sort(arr);
+	System.out.println(arr[0]);
+}
+}
