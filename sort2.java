113a690765bd7979b29179322c5a1f7477a2cad1
Vinoth <38490375+vinothhawk28@users.noreply.github.com>
2018-07-12 17:08:27 +0530

    Update sort2.java

--- sort2.java
+++ sort2.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class sort {
 public static void main(String[] args) {
-int a;
+int b;
 Scanner in=new Scanner(System.in);
-a=in.nextInt();
-int arr[]=new int[a];
-for(int i=0;i<a;i++){
-	arr[i]=in.nextInt();
+b=in.nextInt();
+int arr1[]=new int[b];
+for(int i=0;i<b;i++){
+	arr1[i]=in.nextInt();
 }
-Arrays.sort(arr);
-System.out.println(Arrays.toString(arr));
+Arrays.sort(arr1);
+System.out.println(Arrays.toString(arr1));
 }
 }
