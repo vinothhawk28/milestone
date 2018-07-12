d468d573cee60bbc8c4bdf32a90832e60a37f164
Vinoth <38490375+vinothhawk28@users.noreply.github.com>
2018-07-12 17:55:19 +0530

    Create roman.java

---
+++ roman.java
@@ -0,0 +1,70 @@
+package guvi;
+import java.util.*;
+public class roman {
+public static void main(String[] args) {
+	String a;
+	Scanner in=new Scanner(System.in);
+	a=in.nextLine();
+	if(a.equals("I")){
+	System.out.println(1);
+	}
+	if(a.equals("II")){
+		System.out.println(2);
+		}
+	if(a.equals("III")){
+		System.out.println(3);
+		}
+	if(a.equals("IV")){
+		System.out.println(4);
+		}
+	if(a.equals("V")){
+		System.out.println(5);
+		}
+	if(a.equals("VI")){
+		System.out.println(6);
+		}
+	if(a.equals("VII")){
+		System.out.println(7);
+		}
+	if(a.equals("VIII")){
+		System.out.println(8);
+		}
+	if(a.equals("IX")){
+		System.out.println(9);
+		}
+	if(a.equals("X")){
+		System.out.println(10);
+		}
+	if(a.equals("XI")){
+		System.out.println(11);
+		}
+	if(a.equals("XII")){
+		System.out.println(12);
+		}
+	if(a.equals("XIII")){
+		System.out.println(13);
+		}
+	if(a.equals("XIV")){
+		System.out.println(14);
+		}
+	if(a.equals("XV")){
+		System.out.println(15);
+		}
+	if(a.equals("XVI")){
+		System.out.println(16);
+		}
+	if(a.equals("XVII")){
+		System.out.println(17);
+		}
+	if(a.equals("XVIII")){
+		System.out.println(18);
+		}
+	if(a.equals("XIX")){
+		System.out.println(19);
+		}
+	if(a.equals("XX")){
+		System.out.println(20);
+		}
+
+}
+}
